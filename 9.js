cd22c63cfe8eb5da30b9beba615b87ec4fca8fb9
Prerak126 <prerak126@gmail.com>
2017-08-13 13:58:40 -0400

    Simple 9

---
+++ 9.js
@@ -0,0 +1,20 @@
+var a=prompt("Enter a value in range of 1", "1");
+
+if(a==="1"){
+    window.alert("One");
+}
+else if(a==="2"){
+    window.alert("Two");
+}
+else if(a==="3"){
+    window.alert("Three");
+}
+else if(a==="4"){
+    window.alert("Four");
+}
+else if(a==="5"){
+    window.alert("Five");
+}
+
+else
+    alert("please enter a valid number");
