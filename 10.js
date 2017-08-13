0cfa6decfd6920454415f72ee85132d7f94f5916
Prerak126 <prerak126@gmail.com>
2017-08-13 13:59:38 -0400

    Simple 10

---
+++ 10.js
@@ -0,0 +1,21 @@
+var a=prompt("Enter a number", "1");
+
+if(a>1 && a<10){
+    window.alert("the number is between '1 and 10'")
+}
+
+if(a>11 && a<20){
+    window.alert("the number is between '11 and 20'")
+}
+
+if(a>21 && a<30){
+    window.alert("the number is between '21 and 30'")
+}
+
+if(a>31 && a<40){
+    window.alert("the number is between '31 and 40'")
+}
+
+if(a>41 && a<50){
+    window.alert("the number is between '41 and 50'")
+}
