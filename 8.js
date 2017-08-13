10615aaef6886dd674746cc763c541101a3c7006
Prerak126 <prerak126@gmail.com>
2017-08-13 13:58:10 -0400

    Simple 8

---
+++ 8.js
@@ -0,0 +1,17 @@
+var a=prompt("Enter a value in range of 120", "123");
+
+if(a==="123"){
+    window.alert("One Two Three");
+}
+else if(a==="124"){
+    window.alert("One Two Four");
+}
+else if(a==="125"){
+    window.alert("One Two Five");
+}
+else if(a==="126"){
+    window.alert("One Two Six");
+}
+else if(a==="127"){
+    window.alert("One Two Seven");
+}
