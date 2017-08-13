c868cf8a694c22a77d1607636b6d5aab006a66c6
Prerak126 <prerak126@gmail.com>
2017-08-13 13:57:33 -0400

    Simple 7

---
+++ 7.js
@@ -0,0 +1,32 @@
+var a=prompt("Enter a number from 1 to 5","1");
+
+if(a==="1"){
+    for(var i=2;i<11;i++){
+        document.write(i);
+        document.write("\n");
+    }
+}
+if(a==="2"){
+    for(var i=3;i<12;i++){
+        document.write(i);
+        document.write("\n");
+    }
+}
+if(a==="3"){
+    for(var i=4;i<13;i++){
+        document.write(i);
+        document.write("\n");
+    }
+}
+if(a==="4"){
+    for(var i=5;i<14;i++){
+        document.write(i);
+        document.write("\n");
+    }
+}
+if(a==="5"){
+    for(var i=6;i<15;i++){
+        document.write(i);
+        document.write("\n");
+    }
+}
