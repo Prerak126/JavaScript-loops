1eb2b5d549692d9eaa32d67fd4ac4cebb2765a8b
Prerak126 <prerak126@gmail.com>
2017-08-14 20:09:59 -0400

    Simple 12

---
+++ 12.js
@@ -0,0 +1,21 @@
+var a=prompt("Enter a description like one","One");
+
+if(a==="One"){
+    window.alert("1");
+}
+
+if(a==="Two"){
+    window.alert("2");
+}
+
+if(a==="Three"){
+    window.alert("3");
+}
+
+if(a==="Four"){
+    window.alert("4");
+}
+
+if(a==="Five"){
+    window.alert("5");
+}
