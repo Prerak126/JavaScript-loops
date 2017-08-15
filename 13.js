bd0e1efa1550d90d00da4335577bd7aa5d8e5cc4
Prerak126 <prerak126@gmail.com>
2017-08-14 20:10:40 -0400

    Simple 13

---
+++ 13.js
@@ -0,0 +1,10 @@
+var a=prompt("Enter a number discription ","Six");
+
+var v=["Six","Seven","Eight","Nine","Ten"];
+
+if(a===v[0] || a===v[1] || a===v[2] || a===v[3] || a===v[4]){
+    window.alert("please enter a proper number");
+}
+
+else
+    window.alert("Please enter a valid number");
